cb69c2f8131c377eedafc6055321e615d32304ae
Soumen Ghorai <soumenghorai2802@gmail.com>
2024-12-12 00:02:25 +0530

    added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import "./App.css";
 
-import { Button } from "@/components/ui/button";
+import { Route, Routes } from "react-router-dom";
+
+import { Auth } from "@/pages/Auth/Auth";
+import { SignupCard } from "@/components/organism/Auth/SignupCard";
+import { SigninCard } from "@/components/organism/Auth/SigninCard";
+import { Notfound } from "@/pages/Notfound/Notfound";
 
 function App() {
   return (
-    <div>
-      <Button>Click me</Button>
-    </div>
+    <Routes>
+      <Route
+        path="/auth/signup"
+        element={
+          <Auth>
+            <SignupCard />
+          </Auth>
+        }
+      />
+      <Route
+        path="/auth/signin"
+        element={
+          <Auth>
+            <SigninCard />
+          </Auth>
+        }
+      />
+      <Route path="/*" element={<Notfound />} />
+    </Routes>
   );
 }
 
---
+++ src/components/organism/Auth/SignupCard.jsx
@@ -0,0 +1,90 @@
+import { useState } from "react";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { useNavigate } from "react-router-dom";
+
+export const SignupCard = () => {
+  const navigate = useNavigate();
+
+  const [signupForm, setSignupForm] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+    username: "",
+  });
+
+  return (
+    <Card classname="w-full h-full">
+      <CardHeader>
+        <CardTitle>Signup</CardTitle>
+        <CardDescription>Sign up to access your account</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="space-y-3">
+          <Input
+            placeholder="Email"
+            required
+            onChange={(e) =>
+              setSignupForm({ ...signupForm, email: e.target.value })
+            }
+            value={signupForm.email}
+            type="email"
+            disabled={false}
+          />
+          <Input
+            placeholder="Password"
+            required
+            onChange={(e) =>
+              setSignupForm({ ...signupForm, password: e.target.value })
+            }
+            value={signupForm.password}
+            type="password"
+            disabled={false}
+          />
+          <Input
+            placeholder="Confirm Password"
+            required
+            onChange={(e) =>
+              setSignupForm({ ...signupForm, confirmPassword: e.target.value })
+            }
+            value={signupForm.confirmPassword}
+            type="password"
+            disabled={false}
+          />
+          <Input
+            placeholder="Your username"
+            required
+            onChange={(e) =>
+              setSignupForm({ ...signupForm, username: e.target.value })
+            }
+            value={signupForm.username}
+            type="text"
+            disabled={false}
+          />
+          <Button disabled={false} size="lg" type="submit" className="w-full">
+            Continue
+          </Button>
+        </form>
+        <Separator className="my-5" />
+        <p className="text-s text-muted-foreground mt-4">
+          Already have an account ?{" "}
+          <span
+            className="text-sky-600 hover:underline cursor-pointer"
+            onClick={() => navigate("/auth/signin")}
+          >
+            Sign In
+          </span>
+        </p>
+      </CardContent>
+    </Card>
+  );
+};
